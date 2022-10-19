2599c34bda77acbb16770ad0442ec35091fac7ae
ydragnev <ydragnev@hilscher.com>
2022-10-19 13:52:43 +0300

    Extending

--- Table/Table_2.py
+++ Table/Table_2.py
@@ -7,8 +7,8 @@ class Material(object):
 
 class Wood(Material):
 
-    def __init__(self):
-        super(Wood, self).__init__(strength=10, kind="cedar")
+    def __init__(self, strength=None, kind=None):
+        super(Wood, self).__init__(strength, kind)
 
     def __str__(self):
         return "W"
@@ -16,8 +16,8 @@ class Wood(Material):
 
 class Glass(Material):
 
-    def __init__(self):
-        super(Glass, self).__init__(strength=9, kind="Armored glass")
+    def __init__(self, strength=None, kind=None):
+        super(Glass, self).__init__(strength, kind)
 
     def __str__(self):
         return "G"
@@ -25,50 +25,39 @@ class Glass(Material):
 
 class Metal(Material):
 
-    def __init__(self):
-        super(Metal, self).__init__(strength=15, kind="Aluminium")
+    def __init__(self, strength=None, kind=None):
+        super(Metal, self).__init__(strength, kind)
 
     def __str__(self):
         return "M"
 
 
-# class TableDesign(object):
-#     def __init__(self, table_len, table_width):
+alum = Metal(10, "Aluminium")
+cedar = Wood(5, "Cedar")
+arm_glass = Glass(8, "Armored glass")
+
+table = [[alum, alum, alum, alum, alum],
+         [alum, cedar, arm_glass, cedar, alum],
+         [alum, arm_glass, arm_glass, arm_glass, alum],
+         [alum, cedar, arm_glass, cedar, alum],
+         [alum, alum, alum, alum, alum]
+         ]
+
+table_width = len(table)
+table_len = len(table[0])
+
+for row in range(table_width):
+    for col in range(table_len):
+        print table[row][col]
+        
+
+# class PrintingTable(object):
+#     def __init__(self):
+#         table = TableDesign()
+#         print table
 #
-#         self.table_width = table_width
-#         self.table_len = table_len
 #
-#     table_len = int(input("Enter table length:"))
-#     table_width = int(input("Enter table width:"))
-#
-#     table = []
-#     for width in range(table_width+1):
+#     for row in range(0, table_len):
 #         table.append([])
-#         for length in range(table_len):
-#             wanted_material = str(input("Enter material: "))
-#             if wanted_material == "wood":
-#                 table[width].append(Wood)
-#             elif wanted_material == "metal":
-#                 table[width].append(Metal)
-#             elif wanted_material == "glass":
-#                 table[width].append(Glass)
-#             else:
-#                 raise ValueError ("Not supported material")
-
-
-
-class Printing(object):
-    metal = str(Metal())
-    wood = str(Wood())
-    glass = str(Glass())
-    table_len = 10
-    # table_width = 10
-
-    print (metal * table_len)
-    print (metal + (wood * (table_len - 2)) + metal)
-    print (metal+wood * 3 + glass * 2 + wood * 3 + metal)
-    print (metal+wood*2+glass*4+wood*2+metal)
-    print (metal+wood*2+glass*4+wood*2+metal)
-    print (metal + wood * 3 + glass * 2 + wood * 3 + metal)
-    print (metal + (wood * (table_len - 2)) + metal)
-    print (metal * table_len)
+#         for col in range(0, 10):
+#             table[row].append("")
