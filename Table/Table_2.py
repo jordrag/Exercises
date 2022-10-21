83a189d145c9355950a48136834b88301f7e1760
ydragnev <ydragnev@hilscher.com>
2022-10-21 11:05:24 +0300

    Draft with calc as property of TableDesign.

--- Table/Table_2.py
+++ Table/Table_2.py
@@ -71,18 +71,32 @@ class TableDesign(object):
     def __init__(self, user_table):
         self.table_for_design = user_table
         self.work_table = []
+        self.table_width = len(self.work_table)
+        self.table_len = len(self.work_table[0])
         self.designing_table()
+        self.table_price()
 
     def designing_table(self):
 
-        table_width = len(self.table_for_design)
-        table_len = len(self.table_for_design[0])
-
-        for row in range(table_width):
+        for row in range(self.table_width):
             self.work_table.append([])
-            for col in range(table_len):
+            for col in range(self.table_len):
                 temp_instance = self.table_for_design[row][col]()
                 self.work_table[row].append(temp_instance)
+
+    @property
+    def table_price(self):
+        return self.table_price
+
+    @table_price.setter
+    def table_price(self, value):
+        total = 0
+
+        for row in range(self.table_width):
+            for col in range(self.table_len):
+                temp_instance = self.work_table[row][col]
+                price = temp_instance.price
+                total += price
 
 
 class Printer(object):  # Printing the ready table on the screen
@@ -94,25 +108,25 @@ class Printer(object):  # Printing the ready table on the screen
             print('\t'.join(map(str, item)))
 
 
-class Calculator(object):
-
-    def __init__(self, table_for_calc):
-        self.table_for_calc = table_for_calc
-        self.table_width = len(self.table_for_calc)
-        self.table_len = len(self.table_for_calc[0])
-        self.total_elements = self.table_width * self.table_len
-
-    def calculating_price(self):
-
-        total = 0
-
-        for row in range(self.table_width):
-            for col in range(self.table_len):
-                temp_instance = self.table_for_calc[row][col]
-                price = temp_instance.price
-                total += price
-
-        return total
+# class Calculator(object):
+#
+#     def __init__(self, table_for_calc):
+#         self.table_for_calc = table_for_calc
+#         self.table_width = len(self.table_for_calc)
+#         self.table_len = len(self.table_for_calc[0])
+#         self.total_elements = self.table_width * self.table_len
+#
+#     def calculating_price(self):
+#
+#         total = 0
+#
+#         for row in range(self.table_width):
+#             for col in range(self.table_len):
+#                 temp_instance = self.table_for_calc[row][col]
+#                 price = temp_instance.price
+#                 total += price
+#
+#         return total
 
 
 # ************************************  User interface ********************************************
@@ -129,14 +143,14 @@ user_pattern = [[Aluminium, Aluminium, Aluminium, Aluminium, Aluminium],
 
 
 design = TableDesign(user_pattern)
+price = TableDesign.table_price
 
 Printer(design).printing()
 
 # Calculating the price
+#
+# total_sum = Calculator(design.work_table).calculating_price()
+# total_elements = Calculator(design.work_table).total_elements
+print
+print ("The total price of this table is: {0} lv.".format(price))
 
-total_sum = Calculator(design.work_table).calculating_price()
-total_elements = Calculator(design.work_table).total_elements
-print
-print ("The total price of this table is: {0} lv.".format(total_sum))
-print
-print ("The total elements in this table are: {0} ".format(total_elements))
