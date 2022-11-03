93dc3235560c60bbbcb45b8054a9584d308c3f75
ydragnev <ydragnev@hilscher.com>
2022-11-03 13:10:40 +0200

    working

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,51 +1,77 @@
-username = str(input("Enter username: "))
-difficulty = str(input("Choose difficulty level (easy, medium, hard): "))
-category = str(input("Choose category of words (animals, cars, cities): "))
+import random
 
 
-class UserData(object):
-    usernames_list = {"Ace": 0,"Base": 0,"Case": 0}
+# The database for users, categories and levels in the game
+class Database(object):
+    usernames_list = {"Ace": 5, "Base": 10, "Case": 15}
 
-    def __init__(self, name):
+    categories = {"animals": ["Cow", "Elephant", "Pinguin", "Dog", "Cat", "Dragonfly"],
+                  "cars": ["BMW", "Ford", "Toyota", "Mazda", "Maserati", "Mercedes", "Chevrolet", "Lada"],
+                  "cities": ["Varna", "Bucurest", "Mumbai", "Moscow", "Copenhagen", "Sydney", "Washington"]
+                  }
+
+    levels = {"easy": [3, 5], "medium": [6, 9], "hard": [9, 30]}
+
+
+# Taking specific data from database according to the user entry
+class StartingData(object):
+    def __init__(self, name, cat, diff):
         self.username = name
+        self.difficulty = diff
+        self.category = cat
+
+    @property
+    def game_list(self):
+        temp_list = []
+        min_length = Database.levels[self.difficulty][0]
+        max_length = Database.levels[self.difficulty][1]
+
+        for a in Database.categories[self.category]:
+            if min_length <= len(a) <= max_length:
+                temp_list.append(a)
+        return temp_list
 
     @property
     def hil_points(self):
-        if self.username not in self.usernames_list:
-            self.usernames_list[self.username] = 0
+        if self.username not in Database.usernames_list:
+            Database.usernames_list[self.username] = 0
 
-        return self.usernames_list[self.username]
-
-
-class Category(object):
-    animals = ["Cow","Elephant","Pinguin","Dog","Cat","Dragonfly"]
-    cars = ["BMW","Ford","Toyota","Mazda","Maserati","Mercedes","Chevrolet","Lada"]
-    cities = ["Varna","Bucurest","Mumbai","Moscow","Copenhagen","Sydney","Washington"]
-    temp_list = []
+        return Database.usernames_list[self.username]
 
 
-    def __init__(self, category, difficulty):
-        self.difficulty = difficulty
-        self.category = category
+# User interface
+class UserInput(object):
+    while True:
+        username = str(input("Enter username: "))
+        difficulty = str(input("Choose difficulty level (easy, medium, hard): "))
+        category = str(input("Choose category of words (animals, cars, cities): "))
+        player = StartingData(username, category, difficulty)
 
-        min_length = 0
-        max_length = 0
 
-        if self.difficulty == "easy":
-            min_length = 3
-            max_length = 5
+# Defining the exact word for this game
+class WordMake(object):
+    lett_list = []
 
-        elif self.difficulty == "medium":
-            min_length = 6
-            max_length = 9
+    def __init__(self):
+        self.game_list = UserInput.player.game_list
 
-        elif self.difficulty == "hard":
-            min_length = 9
-            max_length = 30
+    @property
+    def random_word(self):
+        rnd_number = random.randrange(0,len(self.game_list))
+        the_word = self.game_list.pop(rnd_number)
+        return the_word
 
-        for a in category :
-            if min_length <= len(a) <= max_length:
-                self.temp_list.append(a)
+    def word_letters(self):
+        for lett in self.random_word:
+            self.lett_list.append(lett)
+
+
+
+
+
+class Printer(object):
+    print WordMake.random_word
+
 
 class FinalResult(object):
     pass
@@ -55,6 +81,4 @@ class Commands(object):
     pass
 
 
-player_hil_points = UserData(username).hil_points
-words_list = Category(category,difficulty).temp_list
-print words_list
+
